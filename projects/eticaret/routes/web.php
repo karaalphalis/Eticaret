aa115a45296d5b8f3e2445bdd2c269c108dd5416
karaalphalis <karaalp912@gmail.com>
2024-12-15 00:03:36 +0300

    Layout parçalandı

--- projects/eticaret/routes/web.php
+++ projects/eticaret/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return view('');
+});*/
+Route::get('/' ,[FrontController::class,'index']);
+Route::get("/urunler-listesi",[ProductController::class,'liste']);
