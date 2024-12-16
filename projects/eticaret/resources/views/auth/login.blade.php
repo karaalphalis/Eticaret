febc7e1d4fecf836a119a2b8fd4490aa8c28fc81
karaalphalis <karaalp912@gmail.com>
2024-12-16 11:35:51 +0300

    Düzenlemeler Yapılıyor

---
+++ projects/eticaret/resources/views/auth/login.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.auth')
+@section('title', 'Kayıt Ol')
+@push('css') @endpush
+
+
+@section('body')
+    <div class="auth-form-wrapper px-4 py-5">
+        <a href="#" class="noble-ui-logo d-block mb-2">GDG<span>ETicaret</span></a>
+
+        <h5 class="text-muted fw-normal mb-4">Hoş geldiniz. Hesabınıza giriş yapabilirsiniz.</h5>
+        <form class="forms-sample" id="loginForm" action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">Email Adresi</label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="{{ old('email') }}">
+                @error('email')
+                <div class="invalid-feedback d-block">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Parola</label>
+                <input type="password" name="password" class="form-control" id="password" placeholder="Parola">
+            </div>
+            <div class="form-check mb-3">
+                <input type="checkbox" class="form-check-input" id="authCheck" name="remember">
+                <label class="form-check-label" for="authCheck">
+                    Beni Hatırla!
+                </label>
+            </div>
+            <div>
+                <a href="javascript:void(0)" id="btnLogin"
+                   class="btn btn-primary me-2 mb-2 mb-md-0 text-white">Giriş Yap</a>
+{{--                <a href="{{ route('login.socialite', ['driver' => 'google']) }}"--}}
+{{--                   class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">--}}
+{{--                    <i class="mdi mdi-google"></i>--}}
+{{--                    Google İle Giriş Yap--}}
+{{--                </a>--}}
+            </div>
+            <a href="{{ route('register') }}" class="d-block mt-3 text-muted">Hesap Oluştur</a>
+        </form>
+    </div>
+@endsection
+
+@push('js')
+    <script src="{{ asset('assets/js/auth/login.js') }}"></script>
+
+@endpush
--- projects/eticaret/routes/web.php
+++ projects/eticaret/routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\CardController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\MyOrdersController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,13 @@ Route::get("/sepet",[CardController::class,'card']);
 Route::get("/odeme",[CheckoutController::class,'index']);
 Route::get("/siparislerim",[MyOrdersController::class,'index']);
 Route::get("/siparislerim-detay",[MyOrdersController::class,'detail']);
+
+Route::get("kayit-ol",[RegisterController::class,'showForm'])->name("register");
+Route::post("kayit-ol",[RegisterController::class,'register']);
+Route::get("giris",[LoginController::class,'showForm'])->name("login");
+Route::post("giris",[LoginController::class,'login']);
+Route::get("cikis",[LoginController::class,'logout'])->name("logout");
+
 Route::prefix("admin")->group(function(){
     Route::get("/",[DashboardController::class,'index']);
 });
